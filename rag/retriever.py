b1c3f8d2d674dfd44742ef740aa48fba1e56b2b1
Sagarsuman07 <sagarsumanrathore@gmail.com>
2026-09-06 05:39:20 +0530

    implement Phase 6 but caught with error

--- rag/retriever.py
+++ rag/retriever.py
@@ -15,20 +15,66 @@ def retrieve(
 
         return []
 
+
     vector_store = get_vector_store()
 
-    filters = {
-        "user_id": user_id
-    }
 
-    if memory_id is not None:
+    # ========================================================
+    # Global Search
+    # ========================================================
+    #
+    # Search all memories belonging to the current user.
+    #
+    # Example:
+    #
+    # {
+    #     "user_id": "demo_user"
+    # }
+    #
+    # ========================================================
 
-        filters["memory_id"] = memory_id
+    if memory_id is None:
+
+        filters = {
+            "user_id": user_id
+        }
+
+
+    # ========================================================
+    # Memory-Specific Search
+    # ========================================================
+    #
+    # Search only the selected memory belonging to
+    # the current user.
+    #
+    # Chroma requires multiple conditions to be combined
+    # using $and.
+    #
+    # ========================================================
+
+    else:
+
+        filters = {
+            "$and": [
+                {
+                    "user_id": user_id
+                },
+                {
+                    "memory_id": memory_id
+                },
+            ]
+        }
+
+
+    # ========================================================
+    # Semantic Search
+    # ========================================================
 
     results = vector_store.similarity_search_with_score(
         question,
         k=top_k,
         filter=filters,
     )
+
 
     return results
